7f190908f5721e86068a9c083fab4bd3c7501813
harshal Rajnoor <harshalrajnor852056@gmail.com>
2026-05-21 23:23:26 +0530

    initial commit

---
+++ src/main/java/com/harshal/paymentLedgerSystem/PaymentLedgerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.harshal.paymentLedgerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentLedgerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentLedgerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshal/paymentLedgerSystem/PaymentLedgerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshal.paymentLedgerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentLedgerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
